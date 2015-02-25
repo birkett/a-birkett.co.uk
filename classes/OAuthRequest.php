5ff498917413f02747d49ee161971b0c2127f3fe
Anthony Birkett <anthony@a-birkett.com>
2015-02-25 14:33:45 +0000

    Added final missing class doc comments. Added file doc comments to CSS.

--- classes/OAuthRequest.php
+++ classes/OAuthRequest.php
@@ -1,8 +1,44 @@
 <?php
 /**
- * PHP OAuth request class
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Anthony Birkett
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  *
  * PHP Version 5.3
+ *
+ * @category  Classes
+ * @package   PersonalWebsite
+ * @author    Anthony Birkett <anthony@a-birkett.co.uk>
+ * @copyright 2014 Andy Smith
+ * @license   http://opensource.org/licenses/MIT  The MIT License (MIT)
+ * @link      http://www.a-birkett.co.uk
+ */
+
+namespace ABirkett\classes;
+
+/**
+ * OAuth request generation.
+ *
+ * This generated OAuth requests, including the authorisation header.
  *
  * @category  Classes
  * @package   PersonalWebsite
@@ -12,9 +48,6 @@
  * @license   http://opensource.org/licenses/MIT  The MIT License (MIT)
  * @link      http://term.ie
  */
-
-namespace ABirkett\classes;
-
 class OAuthRequest
 {
 
