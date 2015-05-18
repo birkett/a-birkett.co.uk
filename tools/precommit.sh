727af8f120fb027037d6a40eb2b0e09333a79c4f
Anthony Birkett <anthony@a-birkett.com>
2015-05-18 19:01:12 +0100

    Admin style sheet is now generated by SASS.

--- tools/precommit.sh
+++ tools/precommit.sh
@@ -3,5 +3,6 @@
 outstyle='compressed'
 includes='../css/scss'
 
+sassc -I ${includes} -t ${outstyle} ../admin/css/scss/admin.scss ../admin/css/admin.css
 sassc -I ${includes} -t ${outstyle} ../css/scss/main.scss ../css/main.css
 sassc -I ${includes} -t ${outstyle} ../css/scss/christmas.scss ../css/christmas.css
