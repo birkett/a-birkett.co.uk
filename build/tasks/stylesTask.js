82fc95fb687bd5f3e4518854b6fe67142f29fc5b
Anthony Birkett <antbir@gmail.com>
2020-02-22 18:29:16 +0000

    Add version cache breaker tag to images used in CSS.

--- build/tasks/stylesTask.js
+++ build/tasks/stylesTask.js
@@ -1,9 +1,13 @@
 const buildConstants = require('../buildConstants');
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
+const sassVariables = require('gulp-sass-variables');
 
 module.exports = function stylesTask () {
     return src(`${buildConstants.scssInputDirectory}*${buildConstants.scssExtension}`)
+        .pipe(sassVariables({
+            $siteVersion: buildConstants.siteVersion()
+        }))
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(dest(buildConstants.cssOutputDirectory));
 };
