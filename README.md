750309e5e706fafc828e24842feab432891b9e1a
Anthony Birkett <antbir@gmail.com>
2020-02-29 11:26:06 +0000

    Add deployment script.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Simple personal website.
 
 ## Building
-```
+```shell script
 npm install
 npm run build
 ```
@@ -10,6 +10,19 @@ npm run build
 ## Deploying
 After building, the entire ``/dist`` directory can be copied to a simple web server.
 No server side dependencies needed.
+
+## Automated deployment
+A simple deployment script and nginx config is included to automate deployment.
+
+For this to run:
+* SSH must be pre-configured on both the local and remote machine
+* Permissions need to be set on the site root directory
+* Permissions need to be set on the nginx config
+* The user you use on the remote machine should be able to restart nginx
+
+```shell script
+npm run deploy <hostname>
+```
 
 ## Browser Support
 Makes extensive use of SVGs, CSS animations, filters and em units.
---
+++ deploy/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+  echo "Runs on a local machine, copies code to the remote machine, and executes the deployment script on the remote.";
+  echo "Usage: $0 <hostname>";
+
+  return;
+fi
+
+siteName="a-birkett.co.uk";
+timestamp=`date -Iseconds`;
+nginxConfigDir="etc/nginx/sites-available";
+deployDirName=$siteName-$timestamp;
+
+echo "Copying code...";
+scp -r ../dist/ $1:/var/www/$deployDirName;
+
+echo "Copying nginx config...";
+scp $nginxConfigDir/$siteName $1:/$nginxConfigDir/$siteName;
+
+echo "Runing remote deploy script...";
+ssh $1 <<REMOTESCRIPT
+  cd /var/www;
+  echo "Removing symlink...";
+  rm -v $siteName;
+  echo "Creating new symlink...";
+  ln -sv $deployDirName $siteName;
+  echo "Removing old deployments...";
+  ls | grep $siteName | sed '1d;\$d' | xargs rm -rv;
+  echo "Restarting nginx...";
+  sudo systemctl restart nginx;
+REMOTESCRIPT
+
+echo "Deployed $deployDirName";
